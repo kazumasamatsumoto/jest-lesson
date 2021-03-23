c15b3afdcb5b3cd4371d80f0ec60dea547971fca
kazumasamatsumoto <k.matsumoto@benjamin.co.jp>
2021-03-23 18:03:42 +0900

    mock

---
+++ foo.test.js
@@ -0,0 +1,9 @@
+// test.js
+jest.mock('./foo'); // this happens automatically with automocking
+const foo = require('./foo');
+
+test("foo", () => {
+  // foo is a mock function
+  foo.mockImplementation(() => 42);
+  expect(foo()).toBe(42);
+});
--- mock-functions.test.js
+++ mock-functions.test.js
@@ -51,3 +51,102 @@ test("someMockFunction", () => {
   expect(SomeMockConstructor.mock.instances.length).toBe(2);
   expect(SomeMockConstructor.mock.instances[0].name).toEqual("test");
 });
+
+test("mockReturnValueOnce", () => {
+  const myMock = jest.fn();
+  console.log(myMock());
+
+  myMock
+    .mockReturnValueOnce(10)
+    .mockReturnValueOnce("x")
+    .mockReturnValueOnce(true);
+
+  console.log(myMock(), myMock(), myMock(), myMock());
+});
+
+test("filterTest", () => {
+  const filterTestFn = jest.fn();
+
+  filterTestFn.mockReturnValueOnce(true).mockReturnValueOnce(false);
+
+  const result = [11, 12].filter((num) => filterTestFn(num));
+  console.log(result);
+  console.log(filterTestFn.mock.calls);
+});
+
+test("mockImplementation", () => {
+  const myMockFn = jest.fn((cb) => cb(null, true));
+
+  myMockFn((err, val) => console.log(val));
+});
+
+test("mockImplementationOnce", () => {
+  const myMockFn = jest
+    .fn(() => "default")
+    .mockImplementationOnce(() => "first call")
+    .mockImplementationOnce(() => "second call");
+
+  console.log(myMockFn(), myMockFn(), myMockFn(), myMockFn());
+});
+
+test("return this", () => {
+  const myObj = {
+    myMethod: jest.fn().mockReturnThis()
+  };
+
+  const otherObj = {
+    myMethod: jest.fn(function() {
+      return this;
+    })
+  };
+
+  console.log(myObj.myMethod());
+  console.log(otherObj.myMethod());
+});
+
+test("mockName", () => {
+  const myMockFn = jest
+    .fn()
+    .mockReturnValue('default')
+    .mockImplementation(scalar => 42 + scalar)
+    .mockName('add42');
+});
+
+test("custom matchers", () => {
+  const mockFunc = jest.fn();
+
+  const arg1 = "arg1";
+  const arg2 = "arg2";
+
+  mockFunc(arg1, arg2);
+  expect(mockFunc).toBeCalled();
+  expect(mockFunc).toBeCalledWith(arg1, arg2);
+  expect(mockFunc).lastCalledWith(arg1, arg2);
+  expect(mockFunc).toMatchSnapshot();
+})
+
+test("common matchers", () => {
+  const mockFunc = jest.fn().mockName("a mock name");
+
+  const arg1 = 42;
+  const arg2 = "arg2";
+
+  mockFunc(arg1, arg2);
+
+  expect(mockFunc.mock.calls.length).toBeGreaterThan(0);
+  expect(mockFunc.mock.calls).toContainEqual([arg1, arg2]);
+  // The last call to the mock function was called with the specified args
+  expect(mockFunc.mock.calls[mockFunc.mock.calls.length - 1]).toEqual([
+    arg1,
+    arg2
+  ]);
+
+  // The first arg of the last call to the mock function was `42`
+  // (note that there is no sugar helper for this specific of an assertion)
+  expect(mockFunc.mock.calls[mockFunc.mock.calls.length - 1][0]).toBe(42);
+
+  // A snapshot will check that a mock was invoked the same number of times,
+  // in the same order, with the same arguments. It will also assert on the name.
+  expect(mockFunc.mock.calls).toEqual([[arg1, arg2]]);
+  expect(mockFunc.getMockName()).toBe("a mock name");
+})
